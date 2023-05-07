c6d592b350b45ac0335893e7d4e469834bf1f043
LoginovMaxim <loginov.m@azurgames.com>
2023-05-07 15:49:12 +0300

    R! - refactoring

--- Assets/Scripts/App/Fsm/State.cs
+++ Assets/Scripts/App/Fsm/State.cs
@@ -6,23 +6,35 @@ namespace App.Fsm
     public class State : IState
     {
         private readonly ValueType _stateCode;
-        private readonly Action _enterAction;
-        private readonly Action _updateAction;
-        private readonly Action _exitAction;
         private readonly List<ITransition> _transitions;
 
+        private readonly IStateBehaviour _stateBehaviour;
+        
         private State(ValueType stateCode, IStateBehaviour stateBehaviour)
         {
             _stateCode = stateCode;
-            _enterAction = stateBehaviour.OnEnter;
-            _updateAction = stateBehaviour.OnUpdate;
-            _exitAction = stateBehaviour.OnExit;
+            _stateBehaviour = stateBehaviour;
             _transitions = stateBehaviour.GetTransitions();
         }
 
         public static IState NewInstance(ValueType stateCode, IStateBehaviour stateBehaviour)
         {
             return new State(stateCode, stateBehaviour);
+        }
+
+        public void OnEnter()
+        {
+            _stateBehaviour.OnEnter();
+        }
+
+        public void OnUpdate()
+        {
+            _stateBehaviour.OnUpdate();
+        }
+
+        public void OnExit()
+        {
+            _stateBehaviour.OnExit();
         }
 
         public bool TrySwitchOtherState(out ValueType otherStateCode)
@@ -40,21 +52,6 @@ namespace App.Fsm
             }
 
             return false;
-        }
-
-        public void OnEnter()
-        {
-            _enterAction?.Invoke();
-        }
-
-        public void OnUpdate()
-        {
-            _updateAction?.Invoke();
-        }
-
-        public void OnExit()
-        {
-            _exitAction?.Invoke();
         }
 
         #region IState
