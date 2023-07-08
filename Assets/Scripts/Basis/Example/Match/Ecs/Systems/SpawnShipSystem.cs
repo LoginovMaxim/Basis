fcae690867af52922c677383b191f4233f965469
LoginovMaxim <loginov.m@azurgames.com>
2023-07-08 14:20:16 +0300

    B! - fix

--- Assets/Scripts/Basis/Example/Match/Ecs/Systems/SpawnShipSystem.cs
+++ Assets/Scripts/Basis/Example/Match/Ecs/Systems/SpawnShipSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Basis.App.Pool;
+using Basis.Ecs.Common.Components;
 using Basis.Example.Match.Ecs.Components;
 using Basis.Example.Match.Views;
 using Leopotam.EcsLite;
@@ -11,7 +12,10 @@ namespace Basis.Example.Match.Ecs.Systems
         private readonly IPoolService _poolService;
 
         private EcsWorld _world;
+        private EcsFilter _shipTagFilter;
         private EcsPool<ShipTag> _shipTagPool;
+        private EcsPool<PositionSmooth> _positionSmoothPool;
+        private EcsPool<ScaleSmooth> _scaleSmoothPool;
 
         public SpawnShipSystem(IPoolService poolService)
         {
@@ -21,7 +25,12 @@ namespace Basis.Example.Match.Ecs.Systems
         public void Init(IEcsSystems systems)
         {
             _world = systems.GetWorld();
+
+            _shipTagFilter = _world.Filter<ShipTag>().End();
+            
             _shipTagPool = _world.GetPool<ShipTag>();
+            _positionSmoothPool = _world.GetPool<PositionSmooth>();
+            _scaleSmoothPool = _world.GetPool<ScaleSmooth>();
         }
 
         public void Run(IEcsSystems systems)
@@ -39,14 +48,17 @@ namespace Basis.Example.Match.Ecs.Systems
 
             var shipEntityId = _world.NewEntity();
             _shipTagPool.Add(shipEntityId);
-
-            if (!_poolService.TrySpawnView<SampleShipView>(shipEntityId, out var shipView))
-            {
-                return;
-            }
             
-            shipView.Transform.position = new Vector3(Random.Range(0f, 50f), Random.Range(1f, 20f), Random.Range(0f, 50f));
-            shipView.Transform.localScale = Vector3.one * Random.Range(1f, 3f);
+            ref var shipPositionSmooth = ref _positionSmoothPool.Add(shipEntityId);
+            shipPositionSmooth.Value = new Vector3(Random.Range(0f, 50f), Random.Range(1f, 20f), Random.Range(0f, 50f));
+            shipPositionSmooth.Smooth = 2f;
+            
+            ref var shipScaleSmooth = ref _scaleSmoothPool.Add(shipEntityId);
+            shipScaleSmooth.Value = Vector3.one * Random.Range(1f, 3f);
+            shipScaleSmooth.Smooth = 0.5f;
+
+            _poolService.TrySpawnView<SampleShipView>(shipEntityId, out var view);
+            view.Transform.position = shipPositionSmooth.Value;
         }
 
         private void Despawn()
@@ -55,8 +67,13 @@ namespace Basis.Example.Match.Ecs.Systems
             {
                 return;
             }
-            
-            
+
+            foreach (var e in _shipTagFilter)
+            {
+                _poolService.TryDespawnView(e);
+                _world.DelEntity(e);
+                return;
+            }
         }
     }
 }
